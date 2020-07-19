78c6893af8611067cd4231a54577f73380e42eeb
Arsalan <airavani2018@ada.edu.az>
2020-07-19 19:19:48 +0200

    timeout update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 #include <Arduino_APDS9960.h>  // camera
-#include <Arduino_LSM9DS1.h>   // IMU 1111
+#include <Arduino_LSM9DS1.h>   // IMU
 #include <Servo.h>
+#include <Wire.h>
 
 #include "RF24.h"
-// ssss #include "printf.h"
+#include "printf.h"
 
 short delayTime = 500;
 
 // PINS
 
 // ANALOG
-short vibroPin = A0;
+// short vibroPin = A0;
 short photoresistorPin = A1;
 
 // DIGITAL
@@ -45,11 +46,11 @@ boolean timeout = false;
 
 const uint64_t addresses[2] = {0xC1, 0xC2};
 
-byte rollIndex = 0, pitchIndex = 2, yawIndex = 3, throttleIndex = 4;
+byte rollIndex = 0, pitchIndex = 1, yawIndex = 2, throttleIndex = 3;
 byte batteryIndex = 0;
 
-byte transmitData[5];
-byte recievedData[5];
+byte transmitData[1];
+byte recievedData[4];
 
 unsigned long lastRecievedTime = millis();
 unsigned long currentTime = millis();
@@ -63,6 +64,10 @@ void setup() {
   delay(1000);
 
   Serial.println("Started");
+
+  Serial.begin(115200);
+
+  Serial.println("Done with setup!");
 
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
@@ -99,7 +104,7 @@ void setup() {
   radio.setChannel(112);
   radio.printDetails();
 
-  delay(1000);
+  delay(500);
 
   radio.startListening();
 
@@ -237,13 +242,7 @@ void printRecievedData() {
   Serial.println();
 }
 
-// void readSensors() {
-//  transmitData[tData.temperature] = byte(bmp.readTemperature());
-//  transmitData[tData.vibration] = map(analogRead(vibroPin), 0, 1023, 0, 255);
-//  transmitData[tData.altitude] = map(bmp.readAltitude(), 0, 1023, 0, 255);
-//}
-
-//  transmitData[2] = map(bmp.readPressure() / 133.3, 0, 1023, 0, 255);
+void readSensors() { transmitData[batteryIndex] = 0; }
 
 void transmit() {
   Serial.println("Transmitting...");
@@ -270,7 +269,7 @@ void makeStuffWithRecievedData() {
 }
 
 void reset() {
-  Serial.println("Reset...");
+  Serial.println("Resetting...");
   recievedData[rollIndex] = 127;
   recievedData[pitchIndex] = 127;
   recievedData[yawIndex] = 127;
@@ -287,22 +286,31 @@ void ACS() {
   // offset of center of mass.
 }
 
-void printTempHumidity() { delay(1000); }
+void listen() {
+  radio.startListening();
+  if (radio.available()) radio.read(&recievedData, sizeof(recievedData));
+  radio.stopListening();
+}
 
 void loop() {
   // printMotion();  // working
   // printCamera();  // working
-  // printTempHumidity(); //. is not working
   // printPressure();
 
-  timeout = false;
+  readSensors();
+
+  // transmit(); maybe via bluetooth to an iPhone
+
+  delay(delayTime);
+
+  listen();
 
   currentTime = millis();
 
   if (currentTime - lastRecievedTime > 500) {
-    timeout = true;
     Serial.println("TIMEOUT");
     reset();
+    Serial.println();
   }
 
   if (radio.available()) {
