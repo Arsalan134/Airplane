56ef31738dee4b6d8efff2a29d73bab500b1cd7c
Arsalan <airavani2018@gmail.com>
2024-01-05 16:47:16 +0100

    ss

--- src/definitions.h
+++ src/definitions.h
@@ -1,32 +1,33 @@
 #include <Arduino.h>
-
 #include <Servo.h>
-#include <printf.h>
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps612.h"
-#include "RF24.h"
 
-#define isLeonardo
+// #include <Servo.h>
+// #include <printf.h>
+// #include "I2Cdev.h"
+// #include "MPU6050_6Axis_MotionApps612.h"
+// #include "RF24.h"
+
+// #define isLeonardo
 // #define isNano
 
-#ifdef isLeonardo
-#include <Adafruit_DPS310.h>
+// #ifdef isLeonardo
+// #include <Adafruit_DPS310.h>
 // #include <SD.h>
 // #include "bmm150.h"
 // #include "bmm150_defs.h"
-#endif
+// #endif
 
 // orientation/motion vars
-Quaternion q;         // [w, x, y, z]         quaternion container
-VectorInt16 aa;       // [x, y, z]            accel sensor measurements
-VectorInt16 gy;       // [x, y, z]            gyro sensor measurements
-VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;  // [x, y, z]            gravity vector
-float euler[3];       // [psi, theta, phi]    Euler angle container
-float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+// Quaternion q;         // [w, x, y, z]         quaternion container
+// VectorInt16 aa;       // [x, y, z]            accel sensor measurements
+// VectorInt16 gy;       // [x, y, z]            gyro sensor measurements
+// VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
+// VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
+// VectorFloat gravity;  // [x, y, z]            gravity vector
+// float euler[3];       // [psi, theta, phi]    Euler angle container
+// float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-#ifdef isLeonardo
+// #ifdef isLeonardo
 // Magnetometer BMM150
 // BMM150 bmm;
 // bmm150_mag_data bmm150_value;
@@ -34,19 +35,19 @@ float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravi
 // short headingDegrees = 0;
 
 // Barometer DPS310
-Adafruit_DPS310 dps;
-Adafruit_Sensor* dps_temp = dps.getTemperatureSensor();
-Adafruit_Sensor* dps_pressure = dps.getPressureSensor();
-#endif
+// Adafruit_DPS310 dps;
+// Adafruit_Sensor* dps_temp = dps.getTemperatureSensor();
+// Adafruit_Sensor* dps_pressure = dps.getPressureSensor();
+// #endif
 
-MPU6050 mpu;
-uint8_t fifoBuffer[64];  // FIFO storage buffer
+// MPU6050 mpu;
+// uint8_t fifoBuffer[64];  // FIFO storage buffer
 
-RF24 radio(12, 8);
+// RF24 radio(12, 8);
 
-Servo rollLeftMotor;
-Servo rollRightMotor;
-Servo pitchMotor;
+// Servo rollLeftMotor;
+// Servo rollRightMotor;
+// Servo pitchMotor;
 // Servo yawMotor;
 
 Servo engine;
@@ -57,58 +58,59 @@ Servo engine;
 
 boolean timeout = false;
 
-byte addresses[2][6] = {"1Node", "2Node"};
+// byte addresses[2][6] = {"1Node", "2Node"};
 
-byte transmitData[1];
-byte recievedData[7];
+// byte transmitData[1];
+// byte recievedData[7];
 
-unsigned long lastRecievedTime = 0;
+// unsigned long lastRecievedTime = 0;
 
-#define timeoutInMilliSeconds 500
+// #define timeoutInMilliSeconds 500
 
 // Inertial Measurment Unit data
-short currentRollValue = 0;
-short currentPitchValue = 0;
+// short currentRollValue = 0;
+// short currentPitchValue = 0;
 
-// Used by IMU to correct airplane
-short correctedRollAmount = 0;
-short correctedPitchAmount = 0;
+// // Used by IMU to correct airplane
+// short correctedRollAmount = 0;
+// short correctedPitchAmount = 0;
 
-#define multiplierRollACS 3.0
-#define multiplierPitchACS 3.0
+// #define multiplierRollACS 3.0
+// #define multiplierPitchACS 3.0
 
 // #define INTERRUPT_PIN 2
 
-#define pitchServoPin 5
-#define rollServoLeftPin 6
-#define buzzerPin 7
-#define motorPin 9
-#define rollServoRightPin 10
-#define sdCardPin 11
-#define yawServoPin 13
+// #define pitchServoPin 5
+// #define rollServoLeftPin 6
+// #define buzzerPin 7
+#define motorPin 10
+// #define rollServoRightPin 10
+// #define sdCardPin 11
+// #define yawServoPin 13
 
 // Indices in recieve payload
-#define rollIndex 0
-#define pitchIndex 1
-#define yawIndex 2
-#define throttleIndex 3
-#define autopilotIsOnIndex 4
-#define pitchBiasIndex 5
-#define calibrateIndex 6
+// #define rollIndex 0
+// #define pitchIndex 1
+// #define yawIndex 2
+// #define throttleIndex 3
+// #define autopilotIsOnIndex 4
+// #define pitchBiasIndex 5
+// #define calibrateIndex 6
 
 // Indices in transmit payload
-#define batteryIndex 0
+// #define batteryIndex 0
 
-#define delayTime 5
+// #define delayTime 5
 
-#define degreesOfFreedomAilerons 90
+// #define degreesOfFreedomAilerons 90
 
-#define RollLeftBias -20  // negative pitchs down
-#define RollRightBias 20  // negative pitchs down
+// #define RollLeftBias -20  // negative pitchs down
+// #define RollRightBias 20  // negative pitchs down
 
-float pitchBias = 0;
+// float pitchBias = 0;
 
 // Setup
+void calibrate();
 void imuSetup();
 void radioSetup();
 void servoSetup();
